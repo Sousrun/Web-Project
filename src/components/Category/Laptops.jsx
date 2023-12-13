469cd781ed52ddb6ca1d723792aa296a2b67005d
ChetSovisoth <chet.sovisoth@gmail.com>
2023-12-13 12:03:23 +0700

    change img, jsx structure

--- src/components/Category/Laptops.jsx
+++ src/components/Category/Laptops.jsx
@@ -5,147 +5,151 @@ const Laptops = () => {
     return(
 
         <>
-        <div className="content">
-            <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26314/ASUS-ROG-STRIX-G634JY-XS96-XS97-01-400.png?v=a5626834561d7537bf7d113e61a427f44457ddbf"></img>
-            <p><b>AMD Gaming Pc Configurator</b></p> 
-        <p className="text">Window 11 Home AMD Ryzen 9 79900X CPU NVIDIA Geforce RTX 4070 Ti - 12GB 1TB WD M.2 NVMe SSD
-        32GB DDR5-5200MHz RAM</p></div>
-        <button className="buy-1">Buy Now</button>
-        <h1 style={{style: "red"}}><b>$2,289</b></h1>
-        </div>
+            <div className="content">
+                <div> 
+                    <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26314/ASUS-ROG-STRIX-G634JY-XS96-XS97-01-400.png?v=a5626834561d7537bf7d113e61a427f44457ddbf"></img>
+                    <p><b>AMD Gaming Pc Configurator</b></p> 
+                    <p className="text">Window 11 Home AMD Ryzen 9 79900X CPU NVIDIA Geforce RTX 4070 Ti - 12GB 1TB WD M.2 NVMe SSD
+                    32GB DDR5-5200MHz RAM</p>
+                </div>
+                <button className="buy-1">Buy Now</button>
+                <h1 style={{style: "red"}}><b>$2,289</b></h1>
+            </div>
 
-        
-        <div className="content">
-        <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26562/GX650PZ-XS96-01-400-.png?v=a5626834561d7537bf7d113e61a427f44457ddbf"></img>
-        <p><b>Intel Gaming PC Configurator 4</b></p>  
-        <p className="text">Window 11 Home
-            Intel Core i9-13900KF CPU
-            NVIDIA Geforce RTX 4070 Ti - 12GB
-            1TB WD M.2 NVMe SSD
-            32GB DDR5-5200MHz RAM</p></div>
-        <button className="buy-1">Buy Now</button>
-        <h1 style={{style: "red"}}><b>$2,349</b></h1>
-        </div>
+            
+            <div className="content">
+                <div> 
+                    <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26562/GX650PZ-XS96-01-400-.png?v=a5626834561d7537bf7d113e61a427f44457ddbf"></img>
+                    <p><b>Intel Gaming PC Configurator 4</b></p>  
+                    <p className="text">Window 11 Home
+                        Intel Core i9-13900KF CPU
+                        NVIDIA Geforce RTX 4070 Ti - 12GB
+                        1TB WD M.2 NVMe SSD
+                        32GB DDR5-5200MHz RAM</p>
+                </div>
+                <button className="buy-1">Buy Now</button>
+                <h1 style={{style: "red"}}><b>$2,349</b></h1>
+            </div>
 
-        <div className="content">
-        <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26803/G713PV-DS94-01-400-.png?v=a5626834561d7537bf7d113e61a427f44457ddbf"></img>
-        <p><b>AMD Gaming Pc Configurator 4</b></p>  
-        <p className="text">Window 11 Home
-            AMD Ryzen 7 7700X CPU
-            NVIDIA Geforce RTX 4070 Ti - 12GB
-            1TB WD M.2 NVMe SSD
-            32GB DDR5-5200MHz RAM</p></div>
-        <button className="buy-1">Buy Now</button>
-        <h1 style={{style: "red"}}><b>$1,769</b></h1>
-        </div>
+            <div className="content">
+            <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26803/G713PV-DS94-01-400-.png?v=a5626834561d7537bf7d113e61a427f44457ddbf"></img>
+            <p><b>AMD Gaming Pc Configurator 4</b></p>  
+            <p className="text">Window 11 Home
+                AMD Ryzen 7 7700X CPU
+                NVIDIA Geforce RTX 4070 Ti - 12GB
+                1TB WD M.2 NVMe SSD
+                32GB DDR5-5200MHz RAM</p></div>
+            <button className="buy-1">Buy Now</button>
+            <h1 style={{style: "red"}}><b>$1,769</b></h1>
+            </div>
 
-        <div className="content">
-        <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26900/gaming-pc-01-Lancool-216-white-main-400,,,.png?v=b6b1a62f6d50ee3f6b775fde288260934d651b09"></img>
-        <p><b>Intel Gaming Pc Configurator 3</b></p>  
-        <p className="text">Window 11 Home
-            Intel Core i7-13700KF CPU
-            NVIDIA Geforce RTX 4070 Ti - 12GB
-            1TB WD M.2 NVMe SSD
-            32GB DDR5-5200MHz RAM</p></div>
-        <button className="buy-1">Buy Now</button>
-        <h1 style={{style: "red"}}><b>$1,899</b></h1>
-        </div>
+            <div className="content">
+            <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/27054/G614JV-ES94-400-00.png?v=a5626834561d7537bf7d113e61a427f44457ddbf"></img>
+            <p><b>Intel Gaming Pc Configurator 3</b></p>  
+            <p className="text">Window 11 Home
+                Intel Core i7-13700KF CPU
+                NVIDIA Geforce RTX 4070 Ti - 12GB
+                1TB WD M.2 NVMe SSD
+                32GB DDR5-5200MHz RAM</p></div>
+            <button className="buy-1">Buy Now</button>
+            <h1 style={{style: "red"}}><b>$1,899</b></h1>
+            </div>
 
-        <div className="content">
-        <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/23403/gaming-pc-01-SlateMonoMR-main-400.png?v=b6b1a62f6d50ee3f6b775fde288260934d651b09"></img>
-        <p><b>Intel Gaming Pc Configurator 2</b></p>  
-        <p className="text">Window 11 Home
-            Intel Core i5-13600KF CPU
-            NVIDIA Geforce RTX 4070 Ti - 8GB
-            1TB WD M.2 NVMe SSD
-            32GB DDR5-5200MHz RAM</p></div>
-        <button className="buy-1">Buy Now</button>
-        <h1 style={{style: "red"}}><b>$1,459</b></h1>
-        </div>
+            <div className="content">
+            <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/23403/gaming-pc-01-SlateMonoMR-main-400.png?v=b6b1a62f6d50ee3f6b775fde288260934d651b09"></img>
+            <p><b>Intel Gaming Pc Configurator 2</b></p>  
+            <p className="text">Window 11 Home
+                Intel Core i5-13600KF CPU
+                NVIDIA Geforce RTX 4070 Ti - 8GB
+                1TB WD M.2 NVMe SSD
+                32GB DDR5-5200MHz RAM</p></div>
+            <button className="buy-1">Buy Now</button>
+            <h1 style={{style: "red"}}><b>$1,459</b></h1>
+            </div>
 
-        <div className="content">
-        <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26938/gaming-pc-01-FractalRR001-main-400.png?v=b6b1a62f6d50ee3f6b775fde288260934d651b09"></img>
-        <p><b>AMD Gaming Pc Configurator 2</b></p>  
-        <p className="text">Window 11 Home
-            AMD Ryzen 5 7600 CPU
-            NVIDIA Geforce RTX 4070 Ti - 8GB
-            1TB WD M.2 NVMe SSD
-            16GB DDR5-5200MHz RAM</p></div>
-        <button className="buy-1">Buy Now</button>
-        <h1 style={{style: "red"}}><b>$1,549</b></h1>
-        </div>
+            <div className="content">
+            <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26938/gaming-pc-01-FractalRR001-main-400.png?v=b6b1a62f6d50ee3f6b775fde288260934d651b09"></img>
+            <p><b>AMD Gaming Pc Configurator 2</b></p>  
+            <p className="text">Window 11 Home
+                AMD Ryzen 5 7600 CPU
+                NVIDIA Geforce RTX 4070 Ti - 8GB
+                1TB WD M.2 NVMe SSD
+                16GB DDR5-5200MHz RAM</p></div>
+            <button className="buy-1">Buy Now</button>
+            <h1 style={{style: "red"}}><b>$1,549</b></h1>
+            </div>
 
-        <div className="content">
-        <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/27293/gaming-pc-01-Trace7MP003-main-KBM-400-.png?v=b6b1a62f6d50ee3f6b775fde288260934d651b09"></img>
-        <p><b>Gaming RDY EMRRG213</b></p>  
-        <p className="text">Window 11 Home
-            Intel Core i7-13700F CPU
-            Geforce RTX 4060 Ti - 8GB
-            1TB WD M.2 NVMe SSD
-            16GB DDR5-5200MHz RAM</p></div>
-        <button className="buy-1">Buy Now</button>
-        <h1 style={{style: "red"}}><b>$1,149</b></h1>
-        </div>
+            <div className="content">
+            <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/27293/gaming-pc-01-Trace7MP003-main-KBM-400-.png?v=b6b1a62f6d50ee3f6b775fde288260934d651b09"></img>
+            <p><b>Gaming RDY EMRRG213</b></p>  
+            <p className="text">Window 11 Home
+                Intel Core i7-13700F CPU
+                Geforce RTX 4060 Ti - 8GB
+                1TB WD M.2 NVMe SSD
+                16GB DDR5-5200MHz RAM</p></div>
+            <button className="buy-1">Buy Now</button>
+            <h1 style={{style: "red"}}><b>$1,149</b></h1>
+            </div>
 
-        <div className="content">
-        <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26874/gaming-pc-01-Y40BG201-main-400..png?v=6ff1ae6edfd6d9817fe6847b89b1e564143d3733"></img>
-        <p><b>RDY Mori Calliopge</b></p>  
-        <p className="text">Window 11 Home
-            Intel Core i9-13900KF CPU
-            Geforce RTX 4070 Ti - 12GB
-            2TB M.2 NVMe SSD
-            32GB DDR5-6000 T-FORCE RGB RAM</p></div>
-        <button className="buy-1">Buy Now</button>
-        <h1 style={{style: "red"}}><b>$2,299</b></h1>
-        </div>
+            <div className="content">
+            <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26874/gaming-pc-01-Y40BG201-main-400..png?v=6ff1ae6edfd6d9817fe6847b89b1e564143d3733"></img>
+            <p><b>RDY Mori Calliopge</b></p>  
+            <p className="text">Window 11 Home
+                Intel Core i9-13900KF CPU
+                Geforce RTX 4070 Ti - 12GB
+                2TB M.2 NVMe SSD
+                32GB DDR5-6000 T-FORCE RGB RAM</p></div>
+            <button className="buy-1">Buy Now</button>
+            <h1 style={{style: "red"}}><b>$2,299</b></h1>
+            </div>
 
-        <div className="content">
-        <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26969/gaming-pc-01-RDY-Snowfall-main-solo-400.png?v=6ff1ae6edfd6d9817fe6847b89b1e564143d3733"></img>
-        <p><b>RDY Snowfall</b></p>  
-        <p className="text">Window 11 Home
-            Intel Core i9-13900KS CPU
-            ASUS Geforce RTX 4090 Ti - 24GB
-            2TB M.2 NVMe GEN4 SSD
-            32GB DDR5-6000 T-FORCE RGB RAM</p></div>
-        <button className="buy-1">Buy Now</button>
-        <h1 style={{style: "red"}}><b>$4,499</b></h1>
-        </div>
+            <div className="content">
+            <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26969/gaming-pc-01-RDY-Snowfall-main-solo-400.png?v=6ff1ae6edfd6d9817fe6847b89b1e564143d3733"></img>
+            <p><b>RDY Snowfall</b></p>  
+            <p className="text">Window 11 Home
+                Intel Core i9-13900KS CPU
+                ASUS Geforce RTX 4090 Ti - 24GB
+                2TB M.2 NVMe GEN4 SSD
+                32GB DDR5-6000 T-FORCE RGB RAM</p></div>
+            <button className="buy-1">Buy Now</button>
+            <h1 style={{style: "red"}}><b>$4,499</b></h1>
+            </div>
 
-        <div className="content">
-        <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/27073/Gaming-RDY-01-ECL-Pro-III-main-400.png?v=6ff1ae6edfd6d9817fe6847b89b1e564143d3733"></img>
-        <p><b>Gaming RDY ECL Pro III</b></p>  
-        <p className="text">Window 11 Home
-            Intel Core i7-13700KF CPU
-            MSI GeForce RTX 3070-8GB w/iBP WB
-            1TB M.2 NVMe SSD
-            32GB DDR5-6000 T-FORCE RGB RAM</p></div>
-        <button className="buy-1">Buy Now</button>
-        <h1 style={{style: "red"}}><b>$1,499</b></h1>
-        </div>
+            <div className="content">
+            <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/27073/Gaming-RDY-01-ECL-Pro-III-main-400.png?v=6ff1ae6edfd6d9817fe6847b89b1e564143d3733"></img>
+            <p><b>Gaming RDY ECL Pro III</b></p>  
+            <p className="text">Window 11 Home
+                Intel Core i7-13700KF CPU
+                MSI GeForce RTX 3070-8GB w/iBP WB
+                1TB M.2 NVMe SSD
+                32GB DDR5-6000 T-FORCE RGB RAM</p></div>
+            <button className="buy-1">Buy Now</button>
+            <h1 style={{style: "red"}}><b>$1,499</b></h1>
+            </div>
 
-        <div className="content">
-        <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/27074/Gaming-RDY-01-ECL-Pro-Plus-IV-main-400.png?v=6ff1ae6edfd6d9817fe6847b89b1e564143d3733"></img>
-        <p><b>Gaming RDY ECL Pro IV</b></p>  
-        <p className="text">Window 11 Home
-            Intel Core i7-13700KF CPU
-            MSI GeForce RTX 3080-8GB w/iBP WB
-            1TB M.2 NVMe SSD
-            32GB DDR5-6000 T-FORCE RGB RAM</p></div>
-        <button className="buy-1">Buy Now</button>
-        <h1 style={{style: "red"}}><b>$1,899</b></h1>
-        </div>
+            <div className="content">
+            <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/27074/Gaming-RDY-01-ECL-Pro-Plus-IV-main-400.png?v=6ff1ae6edfd6d9817fe6847b89b1e564143d3733"></img>
+            <p><b>Gaming RDY ECL Pro IV</b></p>  
+            <p className="text">Window 11 Home
+                Intel Core i7-13700KF CPU
+                MSI GeForce RTX 3080-8GB w/iBP WB
+                1TB M.2 NVMe SSD
+                32GB DDR5-6000 T-FORCE RGB RAM</p></div>
+            <button className="buy-1">Buy Now</button>
+            <h1 style={{style: "red"}}><b>$1,899</b></h1>
+            </div>
 
-        <div className="content">
-        <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26889/01RDY-Quiet-Night-Main-400-KBM.png?v=6ff1ae6edfd6d9817fe6847b89b1e564143d3733"></img>
-        <p><b>RDY Quiet Night</b></p>  
-        <p className="text">Window 11 Home
-            AMD Ryzen 7 7800X3D CPU
-            GeForce RTX 4070 - 12GB
-            2TB M.2 NVMe SSD
-            32GB DDR5-6000 T-FORCE RGB RAM</p></div>
-        <button className="buy-1">Buy Now</button>
-        <h1 style={{style: "red"}}><b>$1,499</b></h1>
-        </div>
+            <div className="content">
+            <div> <img src="https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26889/01RDY-Quiet-Night-Main-400-KBM.png?v=6ff1ae6edfd6d9817fe6847b89b1e564143d3733"></img>
+            <p><b>RDY Quiet Night</b></p>  
+            <p className="text">Window 11 Home
+                AMD Ryzen 7 7800X3D CPU
+                GeForce RTX 4070 - 12GB
+                2TB M.2 NVMe SSD
+                32GB DDR5-6000 T-FORCE RGB RAM</p></div>
+            <button className="buy-1">Buy Now</button>
+            <h1 style={{style: "red"}}><b>$1,499</b></h1>
+            </div>
         </>
     );
 };
